170f36a58843e0040bea3f73f350846e21eb5e45
Kev <git@kjs1982.me.uk>
2014-12-31 18:28:08 +0000

    As the Arqiva connect streams try to download rather than play lets create a VLC wrapper with a download link for better platforms

---
+++ vlc.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<style type="text/css">
+html, body, embed
+{
+	padding:0;
+	margin:0
+}
+</style>
+<embed type="application/x-vlc-plugin" pluginspage="http://www.videolan.org"version="VideoLAN.VLCPlugin.2"  width="100%" 
+height="100%" id="vlc" loop="yes"autoplay="yes" target="<?=$_GET['url']?>">
+VLC Not Found <br><br>
+Try <a href='<?=$_GET['url']?>'>Clicking Here</a></embed>
