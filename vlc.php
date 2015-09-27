02eb7a0b2ea5c66d98c6de1fd9858c8ad31d6a12
Kev <git@kjs1982.me.uk>
2015-09-27 16:54:42 +0100

    Try to use HTML5 Video Player instead of Embed now VLC's blocked

--- vlc.php
+++ vlc.php
@@ -7,7 +7,10 @@ html, body, embed
 	margin:0
 }
 </style>
-<embed type="application/x-vlc-plugin" pluginspage="http://www.videolan.org"version="VideoLAN.VLCPlugin.2"  width="100%" 
+<video width="100%" height="100%" controls>
+  <source src="<?=$_GET['url']?>" type="video/mp4">
+  <embed type="application/x-vlc-plugin" pluginspage="http://www.videolan.org"version="VideoLAN.VLCPlugin.2"  width="100%" 
 height="100%" id="vlc" loop="yes"autoplay="yes" target="<?=$_GET['url']?>">
-VLC Not Found <br><br>
-Try <a href='<?=$_GET['url']?>'>Clicking Here</a></embed>
+	VLC Not Found <br><br>
+	Try <a href='<?=$_GET['url']?>'>Clicking Here</a></embed>
+</video>
